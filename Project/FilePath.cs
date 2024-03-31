f0f058afaf726ff7696d45e04725f46ab5517aae
Elton Silva <eltoncarsilva73@gmail.com>
2024-03-31 14:46:27 -0300

    fix tilemap sprite path

--- Project/FilePath.cs
+++ Project/FilePath.cs
@@ -3,7 +3,7 @@
     public class FilePath
     {
         // Sprites
-        public const string TILE_MAP_SPRITE_PATH = "Sprits/tilemap";
+        public const string TILE_MAP_SPRITE_PATH = "Sprites/tilemap";
 
         //Atlas
         public const string TILE_MAP_ATLAS_PATH = "Sprites/atlas";
