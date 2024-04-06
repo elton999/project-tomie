257e7df5a985caed13831efc984b166b72f2c26a
Elton Silva <eltoncarsilva73@gmail.com>
2024-04-05 23:21:44 -0300

    rendering text

---
+++ Project/UI/Dialogue.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using UmbrellaToolsKit;
+using UmbrellaToolsKit.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project.UI
+{
+    public class Dialogue : GameObject
+    {
+        private SpriteFont _font;
+        private Point _size;
+
+        private bool _isShowing = false;
+        [ShowEditor] private float _timeToWrite = 0.5f;
+        private Vector2 _letterSize = new Vector2(5, 5);
+
+        private string _text;
+        private string _formattedText;
+        private string _renderText;
+
+        public override void Start()
+        {
+            _size = new Point(200, 30);
+            Position = new Vector2(30, 110);
+            _font = Scene.Content.Load<SpriteFont>("Font");
+            base.Start();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (KeyBoardHandler.KeyPressed(Input.INTERACT))
+                Say("Hello World! it is just a little bit different than you think it is in the game world and you can see it in the game world by clicking on the button below and press the Enter key.");
+            base.Update(gameTime);
+        }
+
+        public void Say(string text)
+        {
+            _isShowing = true;
+            _text = text;
+            _formattedText = "";
+            _renderText = "";
+
+            _text = _text.Trim();
+            _text = _text.Replace("\n", "");
+
+            int lineSize = 0;
+            foreach (string word in _text.Split(" "))
+            {
+                string wordFormatted = word + " ";
+                var size = _font.MeasureString(wordFormatted);
+                if (lineSize + size.X > _size.X)
+                {
+                    wordFormatted = $"\n{wordFormatted}";
+                    lineSize = 0;
+                }
+                _formattedText += wordFormatted;
+                lineSize += (int)size.X;
+            }
+
+            CoroutineManagement.ClearCoroutines();
+            CoroutineManagement.StarCoroutine(WriteText());
+        }
+
+        public IEnumerator WriteText()
+        {
+            for (int i = 0; i < _formattedText.Length; i++)
+            {
+                string currentLetter = _formattedText[i].ToString();
+                _renderText += currentLetter;
+                UmbrellaToolsKit.EditorEngine.Log.Write(_renderText);
+                yield return CoroutineManagement.Wait(_timeToWrite);
+            }
+
+            yield return null;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(
+                SpriteSortMode,
+                BlendState,
+                SamplerState,
+                null,
+                null,
+                Effect,
+                null
+            );
+
+            DrawSprite(spriteBatch);
+            EndDraw(spriteBatch);
+        }
+
+        public override void DrawSprite(SpriteBatch spriteBatch)
+        {
+            if (!_isShowing) return;
+            spriteBatch.DrawString(_font, _renderText, Position, Color.White);
+        }
+    }
+}
