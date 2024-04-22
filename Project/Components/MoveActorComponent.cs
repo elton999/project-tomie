ff747ed8a460198e39f79489791846d8accb0059
Elton Silva <eltoncarsilva73@gmail.com>
2024-04-21 22:11:35 -0300

    minor refactoring

--- Project/Components/MoveActorComponent.cs
+++ Project/Components/MoveActorComponent.cs
@@ -6,10 +6,12 @@ namespace Project.Components
 {
     public class MoveActorComponent : Component
     {
-        [ShowEditor] Vector2 _direction = Vector2.Zero;
-        [ShowEditor] float _velocity = 1f;
+        [ShowEditor] private Vector2 _direction = Vector2.Zero;
+        [ShowEditor] private float _velocity = 1f;
 
         private Actor _actor;
+
+        public bool IsMoving => _direction.LengthSquared() > 0.0f;
 
         public override void Start() => _actor = GameObject.GetActor();
 
