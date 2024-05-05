7e8e9968e3a71274c8562c531f6b4090ab2858b9
Elton Silva <eltoncarsilva73@gmail.com>
2024-05-05 01:58:49 -0300

    created CutScene class

---
+++ Project/UI/CutScene.cs
@@ -0,0 +1,19 @@
+﻿using UmbrellaToolsKit;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project.UI
+{
+    public class CutScene : Dialogue
+    {
+        public struct Frame
+        {
+            public Texture2D Sprite;
+            public string Text;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+    }
+}
