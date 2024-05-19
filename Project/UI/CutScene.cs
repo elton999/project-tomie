0ea4cb623f29caa8f494a6847bd1a6e0564d0977
Elton Silva <eltoncarsilva73@gmail.com>
2024-05-19 16:26:03 -0300

    added logic to set the cutscene data

--- Project/UI/CutScene.cs
+++ Project/UI/CutScene.cs
@@ -1,7 +1,7 @@
-﻿using UmbrellaToolsKit;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using UmbrellaToolsKit.EditorEngine.Windows.DialogueEditor;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Project.UI
 {
@@ -15,17 +15,57 @@ namespace Project.UI
 
         private string _path;
         private DialogueFormat _dialogue;
+        private List<Frame> _frames;
+
+        public List<Frame> Frames => _frames;
 
         public CutScene(string path) => _path = path;
 
         public override void Start()
         {
+            SetDialogues();
+            UpdateFrames();
             base.Start();
+        }
+
+        public void SetDialogues()
+        {
             using (StreamReader stream = new StreamReader(_path))
             {
                 string json = stream.ReadToEnd();
                 _dialogue = DialogueFormat.FromJson(json);
+                stream.Dispose();
             }
+        }
+
+        public void UpdateFrames()
+        {
+            var firstNode = _dialogue.GetFirstNode();
+            _frames = new List<Frame>();
+
+            do
+            {
+                Log($"next node {firstNode.NextNode}");
+                firstNode = _dialogue.GetNodeById(firstNode.NextNode);
+                var firstOption = _dialogue.GetNodeById(firstNode.Options[0]);
+                var sprite = Content.Load<Texture2D>($"CutScenes/{firstNode.Sprite}");
+                string text = firstOption.Content;
+                _frames.Add(new Frame() { Sprite = sprite, Text = text });
+
+                Log(firstNode.Name);
+                Log(text);
+                firstNode = firstOption;
+                Log(firstNode.Id.ToString());
+
+            } while (_dialogue.GetNodeById(firstNode.NextNode).NextNode >= 0);
+            Log("done");
+        }
+
+        private void Log(string value)
+        {
+#if !RELEASE
+            UmbrellaToolsKit.EditorEngine.Log.Write("[Node Frame] " + value);
+#endif
         }
     }
 }
