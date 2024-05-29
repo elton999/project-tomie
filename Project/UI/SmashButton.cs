c6e3c24b98b39357ff855efb88fb858580fbb988
Elton Silva <eltoncarsilva73@gmail.com>
2024-05-28 21:39:27 -0300

    feedback circles integration

--- Project/UI/SmashButton.cs
+++ Project/UI/SmashButton.cs
@@ -20,6 +20,12 @@ namespace Project.UI
 
         private FMOD.Studio.EventInstance _typeEventInstance;
 
+        private Texture2D _circleTexture;
+        private AsepriteDefinitions _circleDefinitions;
+        private Rectangle _circleFilled => _circleDefinitions.Slices["filled_circle"].Item1;
+        private Rectangle _circleUnfilled => _circleDefinitions.Slices["unfilled_circle"].Item1;
+        private Vector2 _circlePosition => (Scene.Sizes.ToVector2() / 2.0f).ToPoint().ToVector2() - Vector2.UnitY * 30;
+
         public float Progress => _progress;
 
         public override void Start()
@@ -28,6 +34,10 @@ namespace Project.UI
             AsepriteDefinitions asepriteDefinitions = Content.Load<AsepriteDefinitions>(FilePath.SMASH_BUTTON_ATLAS_PATH);
             _animation = new AsepriteAnimation(asepriteDefinitions);
             _typeEventInstance = SoundManager.Instance.GetEventInstance(FSPRO.Event.UI_Click);
+
+            _circleTexture = Content.Load<Texture2D>(FilePath.TILE_MAP_SPRITE_PATH);
+            _circleDefinitions = Content.Load<AsepriteDefinitions>(FilePath.TILE_MAP_ATLAS_PATH);
+
             base.Start();
         }
 
@@ -54,12 +64,19 @@ namespace Project.UI
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            if (_cooldown > 0.0f)
-                Body = _animation.Body;
-            else
-                Body = _animation.AsepriteDefinitions.Bodys[0];
             spriteBatch.GraphicsDevice.Clear(Scene.BackgroundColor);
+
+            if (_cooldown > 0.0f) Body = _animation.Body;
+            else Body = _animation.AsepriteDefinitions.Bodys[0];
+
             base.Draw(spriteBatch);
+        }
+
+        public override void DrawSprite(SpriteBatch spriteBatch)
+        {
+            base.DrawSprite(spriteBatch);
+            spriteBatch.Draw(_circleTexture, _circlePosition, _circleUnfilled, Color.White, 1f, (_circleUnfilled.Size.ToVector2() / 2.0f).ToPoint().ToVector2(), 1.0f, spriteEffect, 0);
+            spriteBatch.Draw(_circleTexture, _circlePosition, _circleFilled, Color.White, 1f, (_circleUnfilled.Size.ToVector2() / 2.0f).ToPoint().ToVector2(), Progress, spriteEffect, 0);
         }
 
         private void SetProgress(float progress)
