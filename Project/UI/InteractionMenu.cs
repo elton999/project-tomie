7610e9f4235d9818e91fa76241c5e44bb6ca526a
Elton Silva <eltoncarsilva73@gmail.com>
2024-06-11 22:18:55 -0300

    exit button to hide interaction menu

--- Project/UI/InteractionMenu.cs
+++ Project/UI/InteractionMenu.cs
@@ -33,6 +33,9 @@ namespace Project.UI
             if (KeyBoardHandler.KeyPressed(Input.DOWN))
                 SelectNextButton();
 
+            if (KeyBoardHandler.KeyPressed(Input.EXIT))
+                HideMenu();
+
         }
     }
 }
