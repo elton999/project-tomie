31ec6bb3b0dcb0ac70feae8172b25bdc5b8ca208
Elton Silva <eltoncarsilva73@gmail.com>
2024-08-11 21:32:28 -0300

    basic game settings integrations

--- Project/FilePath.cs
+++ Project/FilePath.cs
@@ -15,12 +15,10 @@
         public const string SMASH_BUTTON_SPRITE_PATH = UI_SPRITES_PATH + "smash_button";
         public const string GAME_LOGO_SPRITE_PATH = UI_SPRITES_PATH + "title_logo";
 
-
         // Particles Effects
         public const string BUTTON_SMASH_FINISH_PARTICLE_1_SPRITE_PATH = BUTTON_SMASH_PARTICLES + "finish_efx1";
         public const string BUTTON_SMASH_FINISH_PARTICLE_2_SPRITE_PATH = BUTTON_SMASH_PARTICLES + "finish_efx2";
         public const string BUTTON_SMASH_FINISH_PARTICLE_3_SPRITE_PATH = BUTTON_SMASH_PARTICLES + "finish_efx3";
-
 
         // Atlas
         public const string TILE_MAP_ATLAS_PATH = SPRITES_PATH + "atlas";
@@ -35,5 +33,9 @@
 
         // CutScenes
         public const string CUTSCENE_1_PATH = @"Content/Dialogues/cutscene_1.dn";
+
+
+        // Properties Settings
+        public const string GAME_DEBUGGER_PATH = @"Content/GameDebuggerSettings.xml";
     }
 }
