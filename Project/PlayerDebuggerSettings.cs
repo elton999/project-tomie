55bf43583088a21db72a4c7836e99c3ab349094d
Elton Silva <eltoncarsilva73@gmail.com>
2024-08-19 21:02:57 -0300

    player debugger property created

---
+++ Project/PlayerDebuggerSettings.cs
@@ -0,0 +1,12 @@
+﻿using UmbrellaToolsKit;
+using UmbrellaToolsKit.EditorEngine.Attributes;
+
+namespace Company.ClassLibrary1
+{
+    [GameSettingsProperty(nameof(PlayerDebuggerSettings), "/Content/")]
+    public class PlayerDebuggerSettings
+    {
+        [ShowEditor] public bool ShowPlayerCollisionArea = false;
+        [ShowEditor] public bool ShowPlayerHitBoxArea = false;
+    }
+}
