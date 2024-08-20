0aa14240b98a2b11552f2c56ca567bdc5101bc7d
Elton Silva <eltoncarsilva73@gmail.com>
2024-08-19 21:00:10 -0300

    created more methods for game settings property class

--- Libs/UmbrellaToolsKit/EditorEngine/GameSettingsProperty.cs
+++ Libs/UmbrellaToolsKit/EditorEngine/GameSettingsProperty.cs
@@ -5,19 +5,20 @@ namespace UmbrellaToolsKit.EditorEngine
 {
     public class GameSettingsProperty
     {
-        public static GameSettingsProperty GetProperty(string pathFile, Type type = null)
+        public static GameSettingsProperty GetProperty(string pathFile, Type type)
         {
             var timer = new Utils.Timer();
             timer.Begin();
 
-            GameSettingsProperty property = (GameSettingsProperty)Activator.CreateInstance(type == null ? typeof(GameSettingsProperty) : type);
+            GameSettingsProperty property = (GameSettingsProperty)Activator.CreateInstance(type);
             using (XmlReader reader = XmlReader.Create(pathFile))
             {
                 try
                 {
                     property = Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate.IntermediateSerializer.Deserialize<GameSettingsProperty>(reader, pathFile);
                 }
-                catch { };
+                catch
+                { };
                 reader.Close();
                 timer.End();
                 Log.Write($"[{nameof(GameSettingsProperty)}] reading: {pathFile}");
@@ -26,6 +27,16 @@ namespace UmbrellaToolsKit.EditorEngine
             timer.End();
             Log.Write($"[{nameof(GameSettingsProperty)}] reading: {pathFile}, timer: {timer.GetTotalSeconds()}");
             return property;
+        }
+
+        public static GameSettingsProperty GetGameSettingsProperty(string pathFile)
+        {
+            return GetProperty(pathFile, typeof(GameSettingsProperty));
+        }
+
+        public static T GetProperty<T>(string pathFile) where T : GameSettingsProperty
+        {
+            return (T)GetProperty(pathFile, typeof(T));
         }
     }
 }
