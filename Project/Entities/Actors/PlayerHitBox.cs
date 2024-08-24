a0291975868fcab99cf832c149a28ee7df079cfe
Elton Silva <eltoncarsilva73@gmail.com>
2024-08-24 02:32:26 -0300

    created player hit box class

---
+++ Project/Entities/Actors/PlayerHitBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UmbrellaToolsKit.Collision;
+
+namespace Project.Entities.Actors
+{
+    public class PlayerHitBox : Actor
+    {
+        public override void Start()
+        {
+            base.Start();
+        }
+    }
+}
